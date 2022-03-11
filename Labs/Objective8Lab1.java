2046516e5d90dfd86a615116bcd8554d44be4d45
Chris Brite <chrismbrite@gmail.com>
2022-03-11 06:21:35 -0700

    Complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for (int i=0; i<11; i+=1){
+ System.out.println(i);
+}
+for (int e=10; e>0; e-=1){
+System.out.println(e);
+}
+  }
+}
