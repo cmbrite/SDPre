e8e04ce94fdabfa3dc127761934efea37a559575
Chris Brite <chrismbrite@gmail.com>
2022-03-10 21:11:08 -0700

    Complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Christopher Brite";
+    int age = 33;
+    String state = "Massachusetts";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
